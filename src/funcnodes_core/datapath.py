207f6743114c22c6306527f496c143705f64b67b
JulianKimmig <julian.kimmig@gmx.net>
2025-06-05 14:33:13 +0200

    fix: enhance done method to handle None breaking_nodes and improve clarity

--- src/funcnodes_core/datapath.py
+++ src/funcnodes_core/datapath.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from weakref import ref
-from typing import TYPE_CHECKING, List
+from typing import TYPE_CHECKING, List, Optional
 from .utils.serialization import JSONEncoder, Encdata
 
 if TYPE_CHECKING:
@@ -49,15 +49,19 @@ class DataPath:
         new_lines = [line.ljust(new_lines_max_length) for line in new_lines]
         return "\n".join(new_lines)
 
-    def done(self):
+    def done(self, breaking_nodes: Optional[List[Node]] = None) -> bool:
         """Mark this DataPath as done, removing it from the src_paths and dst_paths."""
+        if breaking_nodes is None:
+            breaking_nodes = []
         node = self.node_ref()
         if node is None:
+            return True
+        if node in breaking_nodes:
             return True
         if node.in_trigger_soon:
             return False
         for trg_path in self.dst_paths:
-            if not trg_path.done():
+            if not trg_path.done(breaking_nodes=breaking_nodes):
                 return False
         return True
 
