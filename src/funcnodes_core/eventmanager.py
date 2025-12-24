1e131c29c399158b57afe9a81ae6fd25373678c4
Julian Kimmig <julian.kimmig@gmx.net>
2025-12-24 06:41:13 +0100

    chore(eventmanager): slice in wring line, detected on git action

--- src/funcnodes_core/eventmanager.py
+++ src/funcnodes_core/eventmanager.py
@@ -336,8 +336,8 @@ class EventEmitterMixin:
             raise ValueError("src is a reserved keyword")
         msg["src"] = self
         listened = False
-        if event_name in self._events[:]:
-            for callback in self._events[event_name]:
+        if event_name in self._events:
+            for callback in self._events[event_name][:]:
                 callback(**msg)
                 listened = True
         if "*" in self._events:
